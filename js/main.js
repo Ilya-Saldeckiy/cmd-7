ba8302bdbcab338ee147eba92af0b1405e9aed45
saldetskii_ilia <melllllowtoy@yandex.ru>
2021-04-06 17:33:34 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function($) {
 	$('.popup-open').click(function() {
+		$(".logo").addClass("logo-off");
 		$('.popup-fade-1').fadeIn(0);
 		return false;
 	});	
 	
 	$('.popup-close').click(function() {
+		$(".logo").removeClass("logo-off");
 		$(this).parents('.popup-fade-1').fadeOut(0);
 		return false;
 	});		
@@ -13,21 +15,25 @@ $(document).ready(function($) {
 		if (e.keyCode === 27) {
 			e.stopPropagation();
 			$('.popup-fade-1').fadeOut(0);
+			$(".logo").removeClass("logo-off");
 		}
 	});
 	
 	$('.popup-fade-1').click(function(e) {
 		if ($(e.target).closest('.popup-1').length == 0) {
-			$(this).fadeOut(0);					
+			$(this).fadeOut(0);	
+			$(".logo").removeClass("logo-off");				
 		}
 	});
 
 	$('.popup-open-2').click(function() {
+		$(".logo").addClass("logo-off");
 		$('.popup-fade-2').fadeIn(0);
 		return false;
 	});	
 	
 	$('.popup-close').click(function() {
+		$(".logo").removeClass("logo-off");
 		$(this).parents('.popup-fade-2').fadeOut(0);
 		return false;
 	});		
@@ -36,11 +42,13 @@ $(document).ready(function($) {
 		if (e.keyCode === 27) {
 			e.stopPropagation();
 			$('.popup-fade-2').fadeOut(0);
+			$(".logo").removeClass("logo-off");
 		}
 	});
 	
 	$('.popup-fade-2').click(function(e) {
 		if ($(e.target).closest('.popup-2').length == 0) {
+			$(".logo").removeClass("logo-off");
 			$(this).fadeOut(0);					
 		}
 	});
